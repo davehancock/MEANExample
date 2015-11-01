8593bc7cb9023babe2895f243f26336ff5c2b6fc
daves125125 <daves125125@gmail.com>
2015-11-01 18:58:47 +0000

    amending port

--- social/server.js
+++ social/server.js
@@ -13,8 +13,8 @@ app.use(require('./controllers/api/posts'));
 app.use(require('./controllers/api/users'));
 app.use(require('./controllers/api/sessions'));
 
-var server = app.listen(8080, function () {
-    console.log('Server Listening on port:', 8080);
+var server = app.listen(80, function () {
+    console.log('Server Listening on port:', 80);
 })
 
 webSocketServer.connect(server);
